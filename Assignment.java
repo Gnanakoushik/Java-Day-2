f85e8f1b74f677e203ab9588f3689034f1a0c143
Gnanakoushik <jpranathi04@gmail.com>
2024-12-03 11:35:54 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Assignment{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a+=34);
+        System.out.println(a-=4); 
+        System.out.println(a*=3);
+    
+        
+    }
+}
